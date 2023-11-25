301b1b38abbe06fab28d0298046bb9e46524c8df
Den <d.zapekin@gmail.com>
2023-11-25 16:17:39 +0100

    Change path for books.urls

--- library_project/urls.py
+++ library_project/urls.py
@@ -22,7 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('122', include('books.urls')),
+    path('', include('books.urls')),
     path('user/', include('account.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs')
